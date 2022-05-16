30e61ac00ef93bdf9db4bce074088e613d305145
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-05-16 23:14:49 +0300

    Recycler Adapter

--- app/src/main/java/com/example/movieapp/RecyclerAdapter.java
+++ app/src/main/java/com/example/movieapp/RecyclerAdapter.java
@@ -6,20 +6,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
+import java.util.List;
 
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
 
     private static final String TAG = "RecyclerAdapter";
-    int count = 0;
+    List<String> moviesList;
+
+    public RecyclerAdapter(List<String> moviesList) {
+        this.moviesList = moviesList;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Log.i(TAG,"onCraeteViewFolder"+ count++);
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.row_item, parent, false);
         ViewHolder viewHolder = new ViewHolder(view);
@@ -28,15 +33,16 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.rowCountTextView.setText(String.valueOf(position));
+        holder.textView.setText(moviesList.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 20;
+        return moviesList.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         ImageView imageView;
         TextView textView, rowCountTextView;
@@ -48,7 +54,23 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
             textView = itemView.findViewById(R.id.textView);
             rowCountTextView = itemView.findViewById(R.id.rowCountTextView);
 
+            itemView.setOnClickListener(this);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    moviesList.remove(getAdapterPosition());
+                    notifyItemRemoved(getAdapterPosition());
+                    return true;
+                }
+            });
 
         }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(view.getContext(), moviesList.get(getAdapterPosition()), Toast.LENGTH_SHORT).show();
+        }
+    }
 }
-}
+
