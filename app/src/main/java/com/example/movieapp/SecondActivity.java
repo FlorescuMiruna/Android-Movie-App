6a11893ad930731678af7b8a00c10cd477c08005
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-05-21 20:52:10 +0300

    Moved the movie list

--- app/src/main/java/com/example/movieapp/SecondActivity.java
+++ app/src/main/java/com/example/movieapp/SecondActivity.java
@@ -2,14 +2,18 @@ package com.example.movieapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
@@ -31,6 +35,68 @@ public class SecondActivity extends AppCompatActivity {
 // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView2);
        // textView.setText(getString(R.string.hello) + message);
+
+
+        moviesList = new ArrayList<>();
+        moviesList.add("Iron Man");
+        moviesList.add("The Incredible Hulk");
+        moviesList.add("Iron Man 2");
+        moviesList.add("Thor");
+        moviesList.add("Captain America: The First Avenger");
+        moviesList.add("The Avengers");
+        moviesList.add("Iron Man 3");
+        moviesList.add("Thor: The Dark World");
+        moviesList.add("Captain America: The Winter Soldier");
+        moviesList.add("Guardians of the Galaxy");
+        moviesList.add("Avengers: Age of Ultron");
+        moviesList.add("Ant-Man");
+        moviesList.add("Captain America: Civil War");
+        moviesList.add("Doctor Strange");
+        moviesList.add("Guardians of the Galaxy Vol. 2");
+        moviesList.add("Spider-Man: Homecoming");
+        moviesList.add("Thor: Ragnarok");
+        moviesList.add("Black Panther");
+        moviesList.add("Avengers: Infinity War");
+        moviesList.add("Ant-Man and the Wasp");
+        moviesList.add("Captain Marvel");
+        moviesList.add("Avengers: Endgame");
+        moviesList.add("Spider-Man: Far From Home");
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerAdapter = new RecyclerAdapter(moviesList);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(recyclerAdapter);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        bottomNavigationView  = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        goToMainActivity();
+
+                        return true;
+                    case R.id.notification:
+
+                        return true;
+                    case R.id.settings:
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void goToMainActivity(){
+        Intent intent = new Intent(this,SecondActivity.class);
+
+        startActivity(intent);
+
     }
 
 
